b62909ec62aaf50c04486a4e6f91852e00aca3b5
IlhamSambudi <ilham_sambudi_28rpl@student.smktelkom-mlg.sch.id>
2021-08-15 15:45:26 +0700

    login

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -19,6 +19,7 @@ class Filters extends BaseConfig
 		'csrf'     => CSRF::class,
 		'toolbar'  => DebugToolbar::class,
 		'honeypot' => Honeypot::class,
+		'auth'     => App\Filters\Auth::class,
 	];
 
 	/**
@@ -59,4 +60,6 @@ class Filters extends BaseConfig
 	 * @var array
 	 */
 	public $filters = [];
+
+	
 }
